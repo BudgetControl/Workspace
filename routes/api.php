9484349b80e29f11ce6ccf6a16ec7addd9ea32a7
Marco De Felice <marco.defelice@qwentes.it>
2024-05-15 15:05:36 +0200

    workspace share manager

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,12 @@
  * workspace application routes
  */
 $app->get('/{userId}/list', '\Budgetcontrol\Workspace\Controller\WorkspaceController:list');
+$app->get('/{userId}/by-user/list', '\Budgetcontrol\Workspace\Controller\WorkspaceController:listByUser');
 $app->get('/{userId}/last', '\Budgetcontrol\Workspace\Controller\WorkspaceController:last');
 $app->get('/{userId}/{wsId}', '\Budgetcontrol\Workspace\Controller\WorkspaceController:get');
 $app->post('/{userId}/add', '\Budgetcontrol\Workspace\Controller\WorkspaceController:add');
 $app->put('/{userId}/update/{wsId}', '\Budgetcontrol\Workspace\Controller\WorkspaceController:update');
 $app->delete('/{userId}/{wsId}/delete', '\Budgetcontrol\Workspace\Controller\WorkspaceController:delete');
 $app->patch('/{userId}/{wsId}/activate', '\Budgetcontrol\Workspace\Controller\WorkspaceController:activate');
+$app->post('/{userId}/{wsId}/share', '\Budgetcontrol\Workspace\Controller\WorkspaceController:share');
 
