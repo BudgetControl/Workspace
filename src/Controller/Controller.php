edfee9b28806538ff7fc32d4b2c0ca3d8b4fbc0b
Marco De Felice <marco.defelice@qwentes.it>
2024-06-28 13:00:20 +0200

    HOTFIX

--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -10,24 +10,9 @@ class Controller {
 
     public function monitor(Request $request, Response $response)
     {
-        $dbHost = env('DB_HOST');
-        $dbUser = env('DB_USERNAME');
-        $dbPass = env('DB_PASSWORD');
-        $dbName = env('DB_DATABASE');
-
-        // Assuming you are using PDO for database connection
-        try {
-            $pdo = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbPass, $dbUser);
-        } catch (PDOException $e) {
-            // Connection failed
-            $response->getBody()->write('Database connection failed: ' . $e->getMessage());
-            return $response->withStatus(500);
-        }
-
         return response([
             'success' => true,
-            'message' => 'Authentication service is up and running'
+            'message' => 'Workspace service is up and running'
         ]);
-        
     }
 }
