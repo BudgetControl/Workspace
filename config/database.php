be3fa41b9acd2b8956c841819b553d1d371c65e7
Marco De Felice <mrco.defelice@budgetcontrol.cloud>
2024-12-13 12:42:17 +0100

    db configuration

--- config/database.php
+++ config/database.php
@@ -10,5 +10,16 @@ return [
         'charset'  => 'utf8',
         'collation'=> 'utf8_unicode_ci',
         'prefix'   => '',
+    ],
+    
+    'pgsql' => [
+        'driver'   => env('DB_CONNECTION','pgsql'),
+        'host'     => env('DB_HOST'),
+        'database' => env('DB_DATABASE'),
+        'username' => env('DB_USERNAME'),
+        'password' => env('DB_PASSWORD'),
+        'charset'  => 'utf8',
+        'collation'=> 'utf8_unicode_ci',
+        'prefix'   => '',
     ]
 ];
